849a9449ce937c4689f6143a4684f4db195aa36f
Brian Pitts <brian@polibyte.com>
2013-03-24 18:55:59 -0500

    Reduce duplication

--- grallect.rb
+++ grallect.rb
@@ -20,86 +20,18 @@ class Grallect
       :verbose => true,
     }
 
-    @config[:host] = host.gsub!('.', @config[:collectd][:escape_character])
+    escaped_host = host.gsub!('.', @config[:collectd][:escape_character])
+    @host_path = "#{@config[:collectd][:prefix]}#{escaped_host}#{@config[:collectd][:postfix]}"
 
     @logger = Logger.new(STDERR)
     @logger.level = @config[:verbose] ? Logger::DEBUG : Logger::ERROR
   end
 
-  def check_cpu
-    results = []
-    code = nil
-
-    range = (0..@config[:cpu][:count]-1)
-
+  def get_data(graphite_function)
     # number of data points to average together
     samples = @config[:window] / @config[:collectd][:interval]
 
-    # Checking each cpu individually
-    range.each do |i|
-
-      # for each data point, add together the user and system time, then get an average of the data points
-      url = URI.escape("#{@config[:graphite][:url]}/render/?format=json&target=movingAverage(sumSeries(#{@config[:collectd][:prefix]}#{@config[:host]}#{@config[:collectd][:postfix]}.cpu-#{i}.cpu-{user,system}),#{samples})&from=-#{@config[:window]}seconds")
-      @logger.debug URI.unescape(url)
-
-      begin
-        response = open(url).read
-        @logger.debug response
-      rescue SocketError => e
-        @logger.fatal e.message
-        exit 1
-      end
-
-      begin
-        data = JSON.parse(response)
-      rescue ParserError => e
-        @logger.fatal e.message
-        exit 1
-      end
-
-      if data.empty?
-        @logger.warn "No data found at #{url}"
-      else
-        value = data.first['datapoints'].last.first
-        results.push value
-        if value >= @config[:cpu][:warning] and value < @config[:cpu][:critical]
-          code = 1
-        elsif value >= @config[:cpu][:critical]
-          code = 2
-        else
-          code = 0
-        end
-      end
-
-    end
-
-    case code
-    when 0
-      output = 'OK: '
-    when 1
-      output = 'WARNING: '
-    when 2
-      output = 'CRITICAL: '
-    else
-      code = 3
-      output = 'UNKNOWN: No data was found'
-    end
-
-    results.each_with_index { |value,index| output = output + "CPU #{index} averaged #{value}%. " }
-
-    puts output
-    return code
-  end
-
-  def check_memory
-    results = []
-    code = nil
-
-    # number of data points to average together
-    samples = @config[:window] / @config[:collectd][:interval]
-
-    # for each data point, add together the user and system time, then get an average of the data points
-    url = URI.escape("#{@config[:graphite][:url]}/render/?format=json&target=movingAverage(asPercent(#{@config[:collectd][:prefix]}#{@config[:host]}#{@config[:collectd][:postfix]}.memory.memory-{used,free}),#{samples})&from=-#{@config[:window]}seconds")
+    url = URI.escape("#{@config[:graphite][:url]}/render/?format=json&target=movingAverage(#{graphite_function},#{samples})&from=-#{@config[:window]}seconds")
     @logger.debug URI.unescape(url)
 
     begin
@@ -117,20 +49,10 @@ class Grallect
       exit 1
     end
 
-    if data.empty?
-      @logger.warn "No data found at #{url}"
-    else
-      value = data.first['datapoints'].last.first
-      results.push value
-      if value >= @config[:memory][:warning] and value < @config[:memory][:critical]
-        code = 1
-      elsif value >= @config[:memory][:critical]
-        code = 2
-      else
-        code = 0
-      end
-    end
+    return data
+  end
 
+  def output_status(code, results)
     case code
     when 0
       output = 'OK: '
@@ -143,10 +65,59 @@ class Grallect
       output = 'UNKNOWN: No data was found'
     end
 
-    results.each { |value| output = output + "Memory averaged #{value}%. " }
+    results.each { |r| output = output + "#{r[:name]} averaged #{r[:value]}%. " }
 
     puts output
-    return code
+    exit code
+  end
+
+  def check_cpu
+    results = []
+    code = nil
+
+    # Checking each cpu individually
+    range = (0..@config[:cpu][:count]-1)
+    range.each do |i|
+      data = self.get_data("sumSeries(#{@host_path}.cpu-#{i}.cpu-{user,system})")
+      if data.empty?
+        @logger.warn "No data found"
+      else
+        value = data.first['datapoints'].last.first
+        results.push({:name => "CPU #{i}", :value => value})
+        if value >= @config[:cpu][:warning] and value < @config[:cpu][:critical]
+          code = 1
+        elsif value >= @config[:cpu][:critical]
+          code = 2
+        else
+          code = 0
+        end
+      end
+    end
+
+    output_status(code, results)
+  end
+
+  def check_memory
+    results = []
+    code = nil
+
+    data = self.get_data("asPercent(#{@host_path}.memory.memory-{used,free})")
+
+    if data.empty?
+      @logger.warn "No data found"
+    else
+      value = data.first['datapoints'].last.first
+      results.push({:name => "Memory", :value => value})
+      if value >= @config[:memory][:warning] and value < @config[:memory][:critical]
+        code = 1
+      elsif value >= @config[:memory][:critical]
+        code = 2
+      else
+        code = 0
+      end
+    end
+
+    output_status(code, results)
   end
 
 end
