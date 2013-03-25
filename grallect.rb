8294fb171c11175f431ff654dce726b174e5f5fc
Brian Pitts <brian@polibyte.com>
2013-03-24 23:44:07 -0500

    Add interface check

--- grallect.rb
+++ grallect.rb
@@ -16,6 +16,7 @@ class Grallect
       :collectd => { :prefix => nil, :postfix => '.collectd', :escape_character => '_', :interval => 10 },
       :cpu => { :warning => 80, :critical => 95 },
       :memory => { :warning => 80, :critical => 95 },
+      :interface => { :speed => 1000, :warning => 80, :critical => 95 },
       :window => 60,
       :verbose => true,
     }
@@ -70,34 +71,82 @@ class Grallect
     exit code
   end
 
+  def update_code(code, value, warning, critical)
+    case code
+    when 3
+      return 3
+    when 2
+      return 2
+    when 1
+      if value >= critical
+        return 2
+      else
+        return 1
+      end
+    else
+      if value >= warning and value <= critical
+        return 1
+      elsif value >= critical
+        return 2
+      else
+        return 0
+      end
+    end
+
+  end
+
   def check_cpu
     results = []
 
-      user_data = self.get_data("#{@host_path}.cpu-*.cpu-user")
-      system_data = self.get_data("#{@host_path}.cpu-*.cpu-system")
+    # fetching the data seeprately makes parsing results easier
+    user_data = self.get_data("#{@host_path}.cpu-*.cpu-user")
+    system_data = self.get_data("#{@host_path}.cpu-*.cpu-system")
 
-      user_values = user_data.map { |d| d['datapoints'].last.first }
-      system_values = system_data.map { |d| d['datapoints'].last.first }
+    user_values = user_data.map { |d| d['datapoints'].last.first }
+    system_values = system_data.map { |d| d['datapoints'].last.first }
 
-      # array with sum of each pair of user and system data
-      values = [user_values, system_values].transpose.map { |a| a.reduce(:+) }
+    # array with sum of each pair of user and system data
+    values = [user_values, system_values].transpose.map { |a| a.reduce(:+) }
 
-      if values.empty?
-        @logger.warn "No data found"
-        code = 3
-      else
-        values.each_with_index do |value, i|
-          results.push({:label => "CPU #{i} usage percentage", :value => value})
-        end
-        highest = values.sort.last
-        if highest >= @config[:cpu][:warning] and highest < @config[:cpu][:critical]
-          code = 1
-        elsif highest >= @config[:cpu][:critical]
-          code = 2
-        else
-          code = 0
-        end
+    if values.empty?
+      @logger.warn "No data found"
+      code = 3
+    else
+      values.each_with_index do |value, i|
+        results.push({:label => "CPU #{i} usage percentage", :value => value})
       end
+      highest = values.sort.last
+      if highest >= @config[:cpu][:warning] and highest < @config[:cpu][:critical]
+        code = 1
+      elsif highest >= @config[:cpu][:critical]
+        code = 2
+      else
+        code = 0
+      end
+    end
+
+    output_status(code, results)
+  end
+
+  def check_interface
+    results = []
+    code = nil
+
+    data = self.get_data("#{@host_path}.interface-*.if_octets.*")
+
+    if data.empty?
+      @logger.warn "No data found"
+      code = 3
+    else
+      data.each do |d|
+        interface = /interface-(.*?)\./.match(d['target'])[1]
+        direction = /if_octets\.(.*),/.match(d['target'])[1]
+        # convert bytes to megabits
+        value = d['datapoints'].last.first / 131072
+        code = update_code(code, value, @config[:interface][:warning], @config[:interface][:critical])
+        results.push({:label => "Interface #{interface} #{direction} transferred", :value => value})
+      end
+    end
 
     output_status(code, results)
   end
@@ -138,6 +187,8 @@ when 'cpu'
   g.check_cpu
 when 'memory'
   g.check_memory
+when 'interface'
+  g.check_interface
 else
   puts 'What kind of command is that?'
 end
